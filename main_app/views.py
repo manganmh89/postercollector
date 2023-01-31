42d5f69a51d6db3ce58c0a4fdf3ccf1b994cc3d8
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-31 14:13:53 -0500

    adds collection route

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
     return HttpResponse('<h1>Hello!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>All About Poster Collector</h1>')
+    return render(request, 'about.html')
