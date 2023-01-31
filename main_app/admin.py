30cb6fb671b7f7574b9f81d487a6864a556424fd
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-31 17:17:21 -0500

    adds detail and login

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+#import you models here
+from .models import Poster
+
 # Register your models here.
+admin.site.register(Poster)
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from .models import Poster
 
-class Poster:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, band, stock, description, printed):
-    self.band = band
-    self.stock = stock
-    self.description = description
-    self.printed = printed
+# class Poster:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, band, stock, description, printed):
+#     self.band = band
+#     self.stock = stock
+#     self.description = description
+#     self.printed = printed
 
-posters = [
-  Poster('ELO', 'bleached', 'Spaceship, limited print 200', '2019'),
-  Poster('Dead & Co', 'foil', 'Washington, DC special print, 50 printed', '2018'),
-  Poster('Billy Strings', 'natural', 'Covid couch tour, surfing fox', '2021'),
-  Poster("Umphrey's Mcgee", 'bleached', 'Washington DC special, 4 color print', '2017'),
-]
+# posters = [
+#   Poster('ELO', 'bleached', 'Spaceship, limited print 200', '2019'),
+#   Poster('Dead & Co', 'foil', 'Washington, DC special print, 50 printed', '2018'),
+#   Poster('Billy Strings', 'natural', 'Covid couch tour, surfing fox', '2021'),
+#   Poster("Umphrey's Mcgee", 'bleached', 'Washington DC special, 4 color print', '2017'),
+# ]
 
 #Define the home view
 def home(request):
@@ -23,8 +23,9 @@ def about(request):
     return render(request, 'about.html')
 
 def posters_index(request):
+    posters = Poster.objects.all()
     return render(request, 'posters/index.html', { 'posters': posters })
 
-def posters_index(request):
-  posters = Poster.objects.all()
-  return render(request, 'posters/index.html', { 'posters': posters })
+def posters_detail(request, poster_id):
+  poster = Poster.objects.get(id=poster_id)
+  return render(request, 'posters/detail.html', { 'poster': poster })
