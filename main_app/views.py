9c44436287f5229784b78bc9ff7f3d943dbc4d3e
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2023-01-31 13:53:32 -0500

    adds about route

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 #Define the home view
 def home(request):
+    return HttpResponse('<h1>Hello!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>All About Poster Collector</h1>')
